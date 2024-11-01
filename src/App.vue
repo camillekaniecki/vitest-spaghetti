6a5172a2335f910d374a2897df36433eb8859f0e
Camille Kaniecki <ckaniecki@acvauctions.com>
2024-11-01 14:23:39 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container class='fill-height'>
+    <v-responsive class='align-center text-center fill-height'>
+      <div class='demo-wrapper'>
+        TBD
+      </div>
+    </v-responsive>
+  </v-container>
+  <!-- <router-view /> -->
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+.fill-height {
+  min-height: 100vh;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class='fill-height'>
+    <v-responsive class='align-center text-center fill-height'>
+      <div class='demo-wrapper'>
+       <h1>TBD</h1>
+      </div>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+};
+</script>
